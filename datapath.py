37205576ab0c5ea3584b4bb6ff7afc45897fa5e9
Matthew Barlund <100247852+matthewbarlund@users.noreply.github.com>
2025-11-14 21:02:33 -0800

    First commit
    To be implemented in further detail later

---
+++ datapath.py
@@ -0,0 +1,7 @@
+"""
+This file contains the Datapath class
+This will contain a memory_unit, an adder, and a program counter
+The adder will increment the value held by the program counter, which contains the memory address of the instruction, which will be retrieved by the memory unit
+"""
+
+#Note to self: this will have to work with the clock as well
