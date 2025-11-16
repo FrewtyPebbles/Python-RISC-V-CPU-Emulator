5c6bf0a02c70044fdcad2d9528420877d591bf4d
Matthew Barlund <100247852+matthewbarlund@users.noreply.github.com>
2025-11-15 21:24:01 -0800

    Implement Datapath class with instruction handling
    Added Datapath class with methods for instruction retrieval and execution.

--- src/datapath.py
+++ src/datapath.py
@@ -3,5 +3,64 @@ This file contains the Datapath class
 This will contain a memory_unit, an adder, and a program counter
 The adder will increment the value held by the program counter, which contains the memory address of the instruction, which will be retrieved by the memory unit
 """
-
+from memory import Bit, Byte, Memory
+from memory_unit import memory_unit
+from fpu import FPU32
 #Note to self: this will have to work with the clock as well
+class Datapath:
+  mu:memory_unit
+  adder:FPU32
+  
+  def __init__(self, memory_unit, fpu32):
+    self.mu = memory_unit
+    self.adder = fpu32
+
+  def getInstruction(self, address:Bit[32])->string:
+    result = mu.getInstruction(address)
+    return result
+
+  def execute(self, instruction:string):
+    match instruction:
+      #arithmetic  
+      case "add": 
+        pass
+      case "sub":
+        pass
+      case "addi":
+        pass
+      #logical
+      case "and":
+        pass
+      case "or":
+        pass
+      case "xor":
+        pass
+      #shifts
+      case "sll":
+        pass
+      case "srl":
+        pass
+      case "sra":
+        pass
+      #memory
+      case "lw":
+        pass
+      case "sw":
+        pass
+      #control
+      case "beq":
+        pass
+      case "bne":
+        pass
+      case "jal":
+        pass
+      case "jair":
+        pass
+      #immediate/utility
+      case "lui":
+        pass
+      case "auipc":
+        pass
+      #default case
+      case _:
+        raise ValueError("Instruction not found")
