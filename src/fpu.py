1e4942e757c4fcbf3941762e4109fcb171c2d076
Matthew <matthewbarlund@gmail.com>
2025-11-25 13:17:34 -0800

    Added helper method normalize_mul

--- src/fpu.py
+++ src/fpu.py
@@ -47,7 +47,7 @@ class FPU32:
         else:
             res_mantissa = self.alu.op_sub(mantissa_1, mantissa_2)[1]
         
-        res_mantissa, res_exp = self.normalize(res_mantissa, res_exp) #normalize result to 1.F format
+        res_mantissa, res_exp = self.normalize_add(res_mantissa, res_exp) #normalize result to 1.F format
         
         val_mantissa_1 = bin_to_dec(mantissa_1)
         val_mantissa_2 = bin_to_dec(mantissa_2)
@@ -59,9 +59,29 @@ class FPU32:
 
 
     def f_mul(self, operand_1:Bitx32, operand_2:Bitx32) -> Bitx32: 
-        pass
+        sign_1 = operand_1[0] #0 for pos, 1 for neg
+        sign_2 = operand_2[0]
+        res_sign_bit = 0 if sign_1 == sign_2 else 1
 
-    def normalize(self, operand:Bitx32, exponent) -> {Bitx32, int}: 
+        stored_exp_1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_1[1:8] #value of exponent as stored, converted to 32-bit
+        stored_exp_2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_2[1:8]
+
+        mantissa_1 = [0,0,0,0,0,0,0,1] + operand_1[9:31] #normalized value of mantissa in 32-bits, including implied '1' bit
+        mantissa_2 = [0,0,0,0,0,0,0,1] + operand_2[9:31]
+
+        true_exp_1 = bin_to_dec(self.alu.op_sub(stored_exp_1, self.BIAS_BITS)[1]) #remove bias and calculate true value of exponent 
+        true_exp_2 = bin_to_dec(self.alu.op_sub(stored_exp_2, self.BIAS_BITS)[1])
+        true_res_exp = true_exp_1 + true_exp_2
+
+        res_mantissa
+        {res_mantissa, true_res_exp} = self.normalize_mul(res_mantissa, stored_res_exp)
+        stored_res_exp = self.alu.op_add(dec_to_bin_signed(true_res_exp), self.BIAS_BITS)[1][-8:]
+        
+        res:Bitx32 = [res_sign_bit] + stored_res_exp + res_mantissa
+        return res
+    
+
+    def normalize_add(self, operand:Bitx32, exponent) -> {Bitx32, int}: 
         implied_bit = operand[8]
         if implied_bit:
             operand = self.alu.op_sra(operand, self.ONE_32)
@@ -71,6 +91,13 @@ class FPU32:
                 exponent -= 1
                 operand = self.alu.op_sll(operand, self.ONE_32)
         
+        return {operand, exponent}
+    
+    def normalize_mul(self, operand:Bitx32, exponent) -> {Bitx32, int}:
+        implied_bit = operand[8]
+        while not implied_bit:
+            operand = self.alu.op_sra(operand, self.ONE_32)
+            exponent += 1
         return {operand, exponent}
     
     def round_to_nearest_ties_to_even(self, to_be_rounded:Bitx48) -> Bitx23:
