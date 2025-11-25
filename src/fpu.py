6d0ac02240833350cc0c0cb2d1cd65354b0de756
Matthew <matthewbarlund@gmail.com>
2025-11-25 13:44:55 -0800

    Fixed slicing error

--- src/fpu.py
+++ src/fpu.py
@@ -20,11 +20,11 @@ class FPU32:
         sign_1 = operand_1[0] #0 for pos, 1 for neg
         sign_2 = operand_2[0]
 
-        stored_exp_1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_1[1:8] #value of exponent as stored, converted to 32-bit
-        stored_exp_2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_2[1:8]
+        stored_exp_1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_1[1:9] #value of exponent as stored, converted to 32-bit
+        stored_exp_2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_2[1:9]
 
-        mantissa_1 = [0,0,0,0,0,0,0,1] + operand_1[9:31] #normalized value of mantissa in 32-bits, including implied '1' bit
-        mantissa_2 = [0,0,0,0,0,0,0,1] + operand_2[9:31]
+        mantissa_1 = [0,0,0,0,0,0,0,1] + operand_1[9:32] #normalized value of mantissa in 32-bits, including implied '1' bit
+        mantissa_2 = [0,0,0,0,0,0,0,1] + operand_2[9:32]
 
         true_exp_1 = bin_to_dec(self.alu.op_sub(stored_exp_1, self.BIAS_BITS)[1]) #remove bias and calculate true value of exponent 
         true_exp_2 = bin_to_dec(self.alu.op_sub(stored_exp_2, self.BIAS_BITS)[1])
@@ -48,6 +48,7 @@ class FPU32:
             res_mantissa = self.alu.op_sub(mantissa_1, mantissa_2)[1]
         
         res_mantissa, res_exp = self.normalize_add(res_mantissa, res_exp) #normalize result to 1.F format
+        res_mantissa = self.round_to_nearest_ties_to_even([0,0,0,0,0,0,0,0] + res_mantissa)
         
         val_mantissa_1 = bin_to_dec(mantissa_1)
         val_mantissa_2 = bin_to_dec(mantissa_2)
@@ -63,18 +64,19 @@ class FPU32:
         sign_2 = operand_2[0]
         res_sign_bit = 0 if sign_1 == sign_2 else 1
 
-        stored_exp_1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_1[1:8] #value of exponent as stored, converted to 32-bit
-        stored_exp_2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_2[1:8]
+        stored_exp_1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_1[1:9] #value of exponent as stored, converted to 32-bit
+        stored_exp_2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] + operand_2[1:9]
 
-        mantissa_1 = [0,0,0,0,0,0,0,1] + operand_1[9:31] #normalized value of mantissa in 32-bits, including implied '1' bit
-        mantissa_2 = [0,0,0,0,0,0,0,1] + operand_2[9:31]
+        mantissa_1 = [0,0,0,0,0,0,0,1] + operand_1[9:32] #normalized value of mantissa in 32-bits, including implied '1' bit
+        mantissa_2 = [0,0,0,0,0,0,0,1] + operand_2[9:32]
 
         true_exp_1 = bin_to_dec(self.alu.op_sub(stored_exp_1, self.BIAS_BITS)[1]) #remove bias and calculate true value of exponent 
         true_exp_2 = bin_to_dec(self.alu.op_sub(stored_exp_2, self.BIAS_BITS)[1])
         true_res_exp = true_exp_1 + true_exp_2
 
-        res_mantissa
-        {res_mantissa, true_res_exp} = self.normalize_mul(res_mantissa, stored_res_exp)
+        raw_res_mantissa:Bitx48 = binary_mul(mantissa_1, mantissa_2)
+        {raw_res_mantissa, true_res_exp} = self.normalize_mul(raw_res_mantissa, stored_res_exp)
+        res_mantissa:Bitx23 = self.round_to_nearest_ties_to_even(raw_res_mantissa)
         stored_res_exp = self.alu.op_add(dec_to_bin_signed(true_res_exp), self.BIAS_BITS)[1][-8:]
         
         res:Bitx32 = [res_sign_bit] + stored_res_exp + res_mantissa
@@ -93,12 +95,30 @@ class FPU32:
         
         return {operand, exponent}
     
-    def normalize_mul(self, operand:Bitx32, exponent) -> {Bitx32, int}:
+    def normalize_mul(self, operand:Bitx48, exponent) -> {Bitx32, int}:
         implied_bit = operand[8]
         while not implied_bit:
             operand = self.alu.op_sra(operand, self.ONE_32)
             exponent += 1
         return {operand, exponent}
+    
+    def binary_mul(self, operand_1: Bitx23, operand_2: Bitx23) -> Bitx48:
+        res:Bitx48
+        operand_1 = [1] + operand_1
+        operand_2 = [1] + operand_2 #adding implied bit
+
+        partial_products:list
+        for bit in range(24): #for every bit in operand_2
+            if operand_2[bit]:
+                partial_product = operand_1
+                for _ in bit:
+                    partial_product += [0]
+                partial_products.append(partial_product) #if partial product is significant (not zero), store it
+
+        for item in partial_products:
+            res = self.alu.op_add(res[-32:], item) #adding up partial products
+
+        return res
     
     def round_to_nearest_ties_to_even(self, to_be_rounded:Bitx48) -> Bitx23:
         g_bit = to_be_rounded[24]
